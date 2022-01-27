5869882500bec29ed735b90a4deb94530ad3ecdb
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-01-27 17:50:58 -0300

    Created component <LogoLink />

---
+++ src/components/LogoLink/stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { LogoLink, LogoLinkProps } from '.';
+
+export default {
+  title: 'LogoLink',
+  component: LogoLink,
+  args: {
+    text: 'LogoLink',
+    srcImg: 'assets/images/dev-sobrinho.jpeg',
+    link: 'http://localhost',
+  },
+} as Meta;
+
+export const ImageOnly: Story<LogoLinkProps> = (args) => {
+  return (
+    <div>
+      <LogoLink {...args} />
+    </div>
+  );
+};
+
+export const TextOnly: Story<LogoLinkProps> = (args) => {
+  return (
+    <div>
+      <LogoLink {...args} />
+    </div>
+  );
+};
+
+TextOnly.args = {
+  srcImg: '',
+};
