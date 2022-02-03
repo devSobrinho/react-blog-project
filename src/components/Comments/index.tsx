24a31ddf83c93248befee62ce0d3871f6527ef02
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-02-03 14:32:39 -0300

    Build to vercel

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,5 +1,6 @@
 import * as Styled from './styles';
 import { DiscussionEmbed } from 'disqus-react';
+import config from '../../config';
 
 export type CommentsProps = {
   id: string;
@@ -18,7 +19,7 @@ export function Comments({ id, slug, title, allowComments }: CommentsProps) {
       <DiscussionEmbed
         shortname={'blog-dev-sobrinho'}
         config={{
-          url: `/post/${slug}`,
+          url: `${config.url}/post/${slug}`,
           identifier: id,
           title: title,
           language: 'pt-BR',
